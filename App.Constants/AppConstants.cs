7a332c53541e924dfe95982f261df4a2cc3610a3
f14shm4n <uraborg@gmail.com>
2025-10-07 20:53:56 +0300

    reinit.

---
+++ App.Constants/AppConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace App.Constants
+{
+    public static class AppConstants
+    {
+        public const string CalculatorExchangeName = "calExchange";
+        public const string CalculatorQueueName = "operations";
+    }
+}
